0a6cc4c026469ef61b114d2c1f553ba872ae8a55
Linh Nguyen <linh.n@xero.com>
2021-11-29 16:52:46 +1100

    Initial Commit

---
+++ CryptoCurrencyTrackerApp/CryptoCurrencyTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoCurrencyTrackerAppApp.swift
+//  CryptoCurrencyTrackerApp
+//
+//  Created by Linh Nguyen on 29/11/21.
+//
+
+import SwiftUI
+
+@main
+struct CryptoCurrencyTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
